8a49d848a1e3e4d8e62f5eceb143b7038be72775
sindhuri <alladisindhuri@gmail.com>
2026-01-25 14:29:47 +0530

    Create variables.tf file

---
+++ skillup_app/variables.tf
@@ -0,0 +1,20 @@
+variable "tenant_id" {
+    description = "The Tenant ID for the Azure Provider"
+    type        = string
+}
+
+variable "subscription_id" {
+    description = "The Subscription ID for the Azure Provider"
+    type        = string
+}
+
+variable "client_id" {
+    description = "The Client ID for the Azure Provider"
+    type        = string
+}
+
+variable "client_secret" {
+    description = "The Client Secret for the Azure Provider"
+    type        = string
+    
+}
