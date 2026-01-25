770a0db483e06286b637ba4f3fda282defa122fe
sindhuri <alladisindhuri@gmail.com>
2026-01-26 00:21:22 +0530

    Create nsg and add rules in skillup app

---
+++ skillup_app/skillup_nsg.tf
@@ -0,0 +1,25 @@
+resource "azurerm_network_security_group" "skillup_nsg" {
+  name                = "skillup_nsg"
+  location            = "East US"
+  resource_group_name = azurerm_resource_group.skillup_network_rg.name
+
+  tags = {
+    Env   = "prod"
+    Owner = "Sindhuri"
+  }
+}
+
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "deny_all_traffic"
+  priority                    = 1000
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.skillup_network_rg.name
+  network_security_group_name = azurerm_network_security_group.skillup_nsg.name
+}
+
