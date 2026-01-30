f306a0a710611da5319f4c8b8d4741b86d9d0f48
sindhuri <alladisindhuri@gmail.com>
2026-01-30 17:13:34 +0530

    Create route tablefor Skillup

---
+++ skillup_app/Skillup_route_table.tf
@@ -0,0 +1,24 @@
+resource "azurerm_route_table" "Skillup_app_rt" {
+  name                = "Skillup_app_rt"
+  location            = azurerm_resource_group.skillup_network_rg.location
+  resource_group_name = azurerm_resource_group.skillup_network_rg.name
+
+  bgp_route_propagation_enabled = false
+
+  tags = {
+    Env   = "prod"
+    Owner = "Sindhuri"
+  }
+}
+
+# Route Rule: Spoke → Hub via Firewall / NVA
+resource "azurerm_route" "Skillup_to_Flip_via_nva" {
+  name                = "Skillup_to_Flip_via_nva"
+  resource_group_name = azurerm_resource_group.skillup_network_rg.name
+  route_table_name    = azurerm_route_table.Skillup_app_rt.name
+
+  address_prefix         = "172.17.0.0/24"
+  next_hop_type          = "VirtualAppliance"
+  next_hop_in_ip_address = "172.16.0.5"
+  depends_on             = [azurerm_route_table.Skillup_app_rt]
+}
